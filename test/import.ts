16fd8a1b1b4b8bc2107ccba5b36b05ea1384b8c2
Marat Dulin <mdevils@gmail.com>
2023-06-24 23:47:06 +0200

    feat: publish hybrid package: CommonJS and ESM modules

--- test/import.ts
+++ test/import.ts
@@ -1,6 +1,8 @@
 import * as Lib from '../src';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const {ast, render, createParser} = require(process.env.TEST_LIB ? '../lib' : '../src') as typeof Lib;
+const {ast, render, createParser} = require(process.env.TEST_DIST
+    ? `../dist/${process.env.TEST_DIST}`
+    : '../src') as typeof Lib;
 
 export {ast, render, createParser};
